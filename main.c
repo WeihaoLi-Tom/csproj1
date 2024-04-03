894138f928f172cd9f5bab25c410729ef6b7ed58
WeihaoLi-Tom <weihao4@student.unimelb.edu.au>
2024-04-03 13:50:50 +1100

    1
    1

--- main.c
+++ main.c
@@ -11,11 +11,69 @@ typedef struct {
     int finishTime; 
 } Process;
 
+<<<<<<< Updated upstream
+=======
+bool memory[TOTAL_MEMORY] = {false}; 
+
+
+
+void runRoundRobin(Process processes[], int numProcesses, int quantum, int *currentTime, int *completedProcesses) {
+    int currentProcess = -1; 
+    int lastProcess = -1;
+    int quantumCounter = 0;
+
+    while(*completedProcesses < numProcesses) {
+        bool foundProcessToRun = false;
+        for(int i = 0; i < numProcesses; i++) {
+            currentProcess = (currentProcess + 1) % numProcesses;
+
+            int remainingProcesses = 0;
+            for(int j = 0; j < numProcesses; j++) {
+                if(processes[j].startTime <= *currentTime && processes[j].remainingTime > 0) {
+                    remainingProcesses++;
+                }
+            }
+
+            if (*currentTime >= processes[currentProcess].startTime && processes[currentProcess].remainingTime > 0) {
+                foundProcessToRun = true;
+                
+                if (quantumCounter == 0 && currentProcess != lastProcess) {
+                    printf("%d,RUNNING,process-name=%s,remaining-time=%d\n", *currentTime, processes[currentProcess].name, processes[currentProcess].remainingTime);
+                    lastProcess = currentProcess;
+                }
+
+                if (++quantumCounter == quantum) {
+                    processes[currentProcess].remainingTime -= quantum;
+                    *currentTime += quantum;
+
+                    if (processes[currentProcess].remainingTime <= 0) {
+                        processes[currentProcess].finishTime = *currentTime;
+                        (*completedProcesses)++;
+                        printf("%d,FINISHED,process-name=%s,proc-remaining=%d\n", *currentTime, processes[currentProcess].name, remainingProcesses - 1);
+                    }
+
+                    quantumCounter = 0;
+                }
+                break;
+            }
+        }
+
+        if (!foundProcessToRun && quantumCounter == 0) {
+            (*currentTime)++;
+        }
+    }
+}
+
+
+//Command build///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+>>>>>>> Stashed changes
 int main(int argc, char *argv[]) {
     Process processes[2];
     int numProcesses = 0;
     char *filename = NULL;
     int quantum = 0;
+    int currentTime = 0;
+    int completedProcesses = 0;
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) {
@@ -44,8 +102,13 @@ while(fscanf(file, "%d %s %d %*d", &processes[numProcesses].startTime, processes
 }
     fclose(file);
 
+<<<<<<< Updated upstream
     int currentTime = 0;
     int completedProcesses = 0;
+=======
+
+    
+>>>>>>> Stashed changes
 
  
     double maxTimeOverhead = 0.0;
@@ -55,6 +118,7 @@ while(completedProcesses < numProcesses) {
         if(processes[i].remainingTime > 0) {
             if(processes[i].startTime == -1) processes[i].startTime = currentTime;
 
+<<<<<<< Updated upstream
            
             int timeToRun = quantum;
             printf("%d RUNNING process-name=%s remaining-time=%d\n", currentTime, processes[i].name, processes[i].remainingTime);
@@ -79,6 +143,27 @@ while(completedProcesses < numProcesses) {
             }
         }
     }
+=======
+
+//allocate algorithm//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Round Robin////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (strcmp(memoryStrategy, "infinite") == 0) {
+        runRoundRobin(processes, numProcesses, quantum, &currentTime, &completedProcesses);
+    }
+
+
+
+
+
+
+
+
+//First in first out////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+else if (strcmp(memoryStrategy, "first-fit") == 0) {
+
+>>>>>>> Stashed changes
 }
 
  
